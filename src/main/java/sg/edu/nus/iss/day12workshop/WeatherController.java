c8c304416f5239b2f91ef6a5d5c9f04931860dda
augustinecyr <augustinecyr@gmail.com>
2022-07-19 16:29:01 +0800

    final commit

--- src/main/java/sg/edu/nus/iss/day12workshop/WeatherController.java
+++ src/main/java/sg/edu/nus/iss/day12workshop/WeatherController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(path = ("/weather"))
+@RequestMapping(path = {"/weather"})
 public class WeatherController {
 
     @GetMapping
